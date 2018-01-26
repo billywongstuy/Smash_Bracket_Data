45154542b7572b074c76a69116d65c58ee585acd
billywongstuy <billywongstuy@users.noreply.github.com>
2018-01-26 17:30:37 -0500

    Update challonge_api.py

--- challonge_api.py
+++ challonge_api.py
@@ -4,7 +4,7 @@ import urllib2, json
 def get(url,query):
 
     name = url[url.find('challonge.com/')+len('challonge.com/'):]
-    link = 'https://api.challonge.com/v1/tournaments/%s/%s.json?api_key=rX34ZcxUyDVs1jj6PV4lU5a6xvlOn5CD6TUMOQBt' % (name,query)
+    link = 'https://api.challonge.com/v1/tournaments/%s/%s.json?api_key='' % (name,query)
     
     try:
         u = urllib2.urlopen(link)
